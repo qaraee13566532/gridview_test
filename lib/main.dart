0d5a5f4c23f99c1680d0e8e7c34a71c0a51b6de1
Davoud Gharaei <gharaei@takinelectronics.co>
2022-12-07 20:37:05 +0330

    custom toast added.

--- lib/main.dart
+++ lib/main.dart
@@ -50,20 +50,61 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int indexNumber = 0;
+  FToast? fToast;
 
   show(int index) {
-    Fluttertoast.showToast(
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.greenAccent,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.check),
+          SizedBox(
+            width: 12.0,
+          ),
+          Text("Index Number:$index pushed...",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+        ],
+      ),
+    );
+
+    fToast!.showToast(
+      child: toast,
+      toastDuration: Duration(milliseconds: 1500),
+      /* positionedToastBuilder: (context, child) {
+          return Positioned(
+            child: child,
+            top: 32.0,
+            left: 32.0,
+          );
+        }*/
+    );
+    /*  Fluttertoast.showToast(
         msg: "Index #$index pushed...",
+
         toastLength: Toast.LENGTH_SHORT,
+
+
         gravity: ToastGravity.BOTTOM,
         timeInSecForIosWeb: 1,
         backgroundColor: Colors.red,
         textColor: Colors.white,
-        fontSize: 16.0);
+        fontSize: 16.0);*/
   }
 
   _incrementCounter(int index) {
     show(index);
+    print('index = $index');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast!.init(context);
   }
 
   @override
